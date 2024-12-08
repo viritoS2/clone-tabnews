91caf9d8bcbb2cab7b930f650e0c14173fd1762c
vitor <vitoramorimof@gmail.com>
2024-12-08 18:41:10 -0300

    add `databse.js`  and `.env`  file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response){
+import  database  from "/home/vitor/code/clone-tabnews/infra/database.js";
+
+async function status(request, response){
+
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows)
   response.status(200).json({chave: "São acima da média"})
 }
 export default status
