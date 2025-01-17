c910cd526746da7007394f280d7b85b95e5d5547
vitor <vitoramorimof@gmail.com>
2025-01-17 17:34:28 -0300

    make database.js more robust error handling

--- infra/database.js
+++ infra/database.js
@@ -13,10 +13,16 @@ async function query(queryObject){
 
 
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-
-  return result;
+  
+  try{
+    const result = await client.query(queryObject);
+    return result;
+  } catch(error){
+    console.error(error)
+  }
+  finally{
+    await client.end();
+  }
 }
 
 export default {
